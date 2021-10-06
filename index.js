2784edac051ca11641c37bd7e34146003ce263f9
phuoc <95vien2@gmail.com>
2021-10-06 23:21:50 +0200

    add commands

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'claim') {
+		await interaction.reply('Ha! You got me!');
+	} else if (commandName === 'wormtop') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(token);
