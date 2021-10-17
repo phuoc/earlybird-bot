ff960eb7fe2aab9295d3357e0960749b9ff9e835
phuoc <95vien2@gmail.com>
2021-10-17 15:13:40 +0200

    Update leaderboard

--- backlog.js
+++ backlog.js
@@ -94,3 +94,35 @@ run: async (client, message, args) => {
 
 },
 }
+
+//! leaderboard original
+let profiles = await profileModel.find({}, { _id: 0, username: 1, worms: 1 }).sort({ worms: -1 });
+    let worms = [];
+    let username = [];
+
+    for(const profile of profiles) {
+      worms = profiles.map(profile => profile.worms.toString());
+      username = profiles.map(profile => profile.username.toString());
+    }
+
+    let testS = worms[0].toString();
+    
+    const topEmbed = new MessageEmbed()
+      .setColor("#00FF00")
+      .setAuthor(
+        `Leaderboard for ${interaction.guild.name}`,
+        interaction.guild.iconURL({ dynamic: true })
+      )
+      .setThumbnail(
+        "https://cdna.artstation.com/p/assets/images/images/012/471/678/original/austin-lutz-backpack-turnaround.gif?1534960441"
+      ).setTimestamp();
+    
+    for(let i = 0; i < worms.length; i++) {
+      topEmbed.addFields(
+        { name: "Name", value: username[i], inline: true },
+        { name: "Worms", value: worms[i].toString(), inline: true }
+      )
+    }
+
+    interaction.channel.send({ embeds: [topEmbed] });
+    interaction.reply("nICE");
--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -10,42 +10,27 @@ module.exports = {
     .setName("leaderboard")
     .setDescription("Shows leaderboard"),
   async execute(interaction) {
-    let profiles = await profileModel.find({}, { _id: 0, userID: 1, worms: 1 }).sort({ worms: -1 });
-    let worms = "";
-    let userID = "";
+    let profiles = await profileModel.find({}, { _id: 0, username: 1, worms: 1 }).sort({ worms: -1 });
+    let userNames = '';
+    let worms = '';
 
+    for (let i = 0; i < profiles.length; i++) {
+      const data = profiles[i];
 
-    for(const profile of profiles) {
-
-      worms = profiles.map(profile => profile.worms);
-      userID = profiles.map(profile => profile.userID);
-      
+      userNames += `\`${i + 1}\` ${data.username}\n`;
+      worms += `\`${data.worms}\`\n`;
     }
 
-    
-    console.log(worms);
-    console.log(userID);
-
-    const topEmbed = new MessageEmbed()
+    const embed = new MessageEmbed()
+      .setAuthor(`Earliest birds of ${interaction.guild.name}`, interaction.guild.iconURL({ dynamic: true }))
       .setColor("#00FF00")
-      .setTitle("Some title")
-      .setAuthor(
-        `Leaderboard for ${interaction.guild.name}`,
-        interaction.guild.iconURL({ dynamic: true })
-      )
-      .setThumbnail(
-        "https://cdna.artstation.com/p/assets/images/images/012/471/678/original/austin-lutz-backpack-turnaround.gif?1534960441"
-      )
+      .setThumbnail("https://cdna.artstation.com/p/assets/images/images/012/471/678/original/austin-lutz-backpack-turnaround.gif?1534960441")
       .addFields(
-        // { name: "\u200B", value: "\u200B" },
-        // { name: "Name", value: userID, inline: true },
-        // { name: "Worms", value: worms, inline: true }
-      )
-      // .addField("Siste tittel", "Siste tekst", true)
-      .setTimestamp();
-    // .setFooter("Some footer text here", "https://i.imgur.com/AfFp7pu.png");
+        { name: 'User', value: userNames, inline: true },
+        { name: 'Worms', value: worms, inline: true }
+      );
 
-    interaction.channel.send({ embeds: [topEmbed] });
+    interaction.channel.send({ embeds: [embed] });
     interaction.reply("nICE");
   },
 };
