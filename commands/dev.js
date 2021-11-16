cf7d8b9085e547d2874370038a9950f068bfc7d6
phuoc <95vien2@gmail.com>
2021-11-17 00:17:10 +0100

    Add timestamps for global schema

--- commands/dev.js
+++ commands/dev.js
@@ -2,12 +2,15 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const profileModel = require("../models/profileSchema");
 const globalModel = require("../models/globalSchema");
 const { authProfile, getDailyClaim, setDailyClaim } = require("../utils.js");
+const { DataManager } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dev')
 		.setDescription('Add testdata to db'),
 	async execute(interaction) {
+		// let gl = await globalModel.create({globalId: 404, dailyCount: 0});
+
 		const addWorm = await profileModel.findOneAndUpdate({userID: interaction.user.id},{$inc: {worms: 1}});
 		const addCounter = await globalModel.findOneAndUpdate({globalId: 404}, {$inc: {dailyCount: 1}});
 		const dCount = await globalModel.findOne({ globalId: 404}, {dailyCount: 1});
--- models/globalSchema.js
+++ models/globalSchema.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-const globalSchema = new mongoose.Schema({
+const globalSchema = new mongoose.Schema(
+  {
+  globalId: { type: Number, default: 404},
   dailyCount: { type: Number, default: 0 },
-});
+  },
+  { 
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("global", globalSchema);
