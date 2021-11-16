29736ae076ad8bb3f4b00c1aae001f0d393b11a6
phuoc <95vien2@gmail.com>
2021-11-16 15:29:11 +0100

    Update cron job

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ for (const file of commandFiles) {
 client.once('ready', () => {
 	console.log('Worm bot is online!');
 
-	let schedCacheReset = new cron.CronJob('00 28 15 * * *', () => {
+	let schedCacheReset = new cron.CronJob('00 30 15 * * *', () => {
 		resetDaily(profileModel, globalModel);
 	});
 
