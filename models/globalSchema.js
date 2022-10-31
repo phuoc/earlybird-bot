909f15010b6ae200645e3ec372af677191bda6dc
Foko <foko@DESKTOP-RAEE4C9.localdomain>
2022-11-01 00:46:04 +0100

    Changed dailyCount to 1

--- models/globalSchema.js
+++ models/globalSchema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const globalSchema = new mongoose.Schema(
   {
   globalId: { type: Number, default: 404},
-  dailyCount: { type: Number, default: 0 },
+  dailyCount: { type: Number, default: 1 },
   },
   { 
     timestamps: true,
